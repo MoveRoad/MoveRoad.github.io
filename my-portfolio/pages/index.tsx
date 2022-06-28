03f4ba7de1e7bba89ae30a0b2862594783a0a581
이동길 <oio3215@naver.com>
2022-06-28 21:31:54 +0900

    Setting : next 환경 설치, 파비콘, 타이틀 설정

---
+++ my-portfolio/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>DG's Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon_chick.ico" />
+      </Head>
+      <div className=""></div>
+      <style jsx>{``}</style>
+    </>
+  );
+};
+
+export default Home;
