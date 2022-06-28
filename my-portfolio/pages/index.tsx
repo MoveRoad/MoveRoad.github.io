a8ced0b2ea5e6ee82906628e3e53c758028c23e8
MoveRoad <oio3215@naver.com>
2022-06-29 01:28:27 +0900

    Test : github Actions 배포 테스트

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="shortcut icon" href="/favicon_chick.ico" />
       </Head>
-      <div className=""></div>
+      <div className="">테스트</div>
       <style jsx>{``}</style>
     </>
   );
