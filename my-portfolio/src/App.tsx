db53bada4cbb4140c667d84b7810665cea264af3
MoveRoad <oio3215@naver.com>
2022-07-01 22:33:51 +0900

    Fix : 타입스크립트로 변경

---
+++ my-portfolio/src/App.tsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import style from "styled-components";
+import GlobalStyles from "./Theme/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
+}
+
+/*
+from '-components
+"top:142px;left:0;"
+"top:252px;left:157px;"
+"top:218px;left:320px;"
+"top:0;left:397px;"
+"top:110px;left:598px;"
+"top:292px;left:713px;"
+"top:143px;left:805px;"
+"top:136px;left:900px;"
+"top:142px;left:0;"
+"top:252px;left:157px;"
+"top:218px;left:320px;"
+"top:0;left:397px;"
+"top:200px;left:1200px;"
+"top:292px;left:713px;"
+"top:143px;left:805px;"
+"top:136px;left:900px;"
+di
+*/
+
+export default App;
---
+++ my-portfolio/src/Theme/GlobalStyles.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    html, body, div, p {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`;
+
+export default GlobalStyles;
