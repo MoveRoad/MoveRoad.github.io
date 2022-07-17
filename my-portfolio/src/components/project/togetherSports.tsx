a18a7bc064fe04cdebb5f82b17c4615548fee80d
MoveRoad <oio3215@naver.com>
2022-07-17 20:42:43 +0900

    Feature : 이력서 업데이트

--- my-portfolio/src/components/project/togetherSports.tsx
+++ my-portfolio/src/components/project/togetherSports.tsx
@@ -15,7 +15,10 @@ export const TogetherSports = () => {
         설계부터 배포까지 분배하여 협업하였습니다.
       </li>
       <h3> 담당 업무 </h3>
-      <li> Websocket을 이용하여 방 내 실시간 통신 </li>
+      <li>
+        Websocket을 이용하여 방 내 실시간 통신 <br />( 채팅, 방 수정 시 정보
+        업데이트, 방 접속자 확인 및 온/오프라인 관리 등 )
+      </li>
       <Video
         src="/websocket.mp4"
         autoPlay
@@ -23,11 +26,19 @@ export const TogetherSports = () => {
         muted
         poster="/room.png"
       ></Video>
-      <li> 방 목록 필터 기능 및 무한 스크롤링</li>
-      <li> 마이룸 화면의 캐러셀(슬라이더) 및 캘린더 기능</li>
+      <li> auth0 기능과 JWT를 이용한 로그인 상태 관리 구현</li>
+      <li>
+        Infinite scrolling 기능 ( Grid 형식의 방 목록과 Chatting ) 및 Throttling
+        적용
+      </li>
+      <li> Slider(Carousel) 및 Calendar 기능 자체 구현</li>
       <li> 회원정보 & 수정 & 방 생성</li>
-      <li> 카카오 맵 API를 이용해 지역 선택 기능</li>
-      <li> Auth0 기능과 Refresh Token을 이용한 회원관리 기능</li>
+      <li>
+        Map API를 통해 활동지역을 선택하는 기능 및 회원정보 수정 시 기존
+        활동지역 재표시 기능 구현
+      </li>
+      <li>방 목록 조건 필터 기능</li>
+      <li>axios를 통한 API 설계</li>
       <li> 각종 Modal들 </li>
       <h3> 사용한 기술 스택 </h3>
       <li>Next.js</li>
